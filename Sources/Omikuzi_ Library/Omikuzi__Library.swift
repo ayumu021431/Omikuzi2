326f3315f643f62265bb6cdc22e5c76a9d5e17be
ayumu021431 <ayumu_skywalker@icloud.com>
2022-03-19 17:55:36 +0900

    初コミット

--- Sources/Omikuzi_ Library/Omikuzi__Library.swift
+++
@@ -1,6 +0,0 @@
-public struct Omikuzi__Library {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Sources/Omikuzi_ Library/おみくじ.swift
@@ -0,0 +1,33 @@
+//
+//  おみくじ.swift
+//  
+//
+//  Created by asora on 2022/03/19.
+//
+
+import Foundation
+
+public enum おみくじ: String, CaseIterable {
+    case 大吉
+    case 中吉
+    case 小吉
+    case 吉
+    case 末吉
+    case 凶
+    case 大凶
+}
+
+extension おみくじ {
+
+    public static func 引く() -> Self {
+        allCases.randomElement()!
+    }
+}
+
+extension おみくじ: CustomStringConvertible {
+    
+    public var description: String {
+        rawValue
+    }
+
+}
